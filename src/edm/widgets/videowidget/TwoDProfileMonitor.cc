87e103cdf5198d6d8048f08a88c808d9b5d22f72
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2019-02-21 10:37:40 +0000

    Handle short specific type in widget TwoDProfileMonitor

--- src/edm/widgets/videowidget/TwoDProfileMonitor.cc
+++ src/edm/widgets/videowidget/TwoDProfileMonitor.cc
@@ -859,8 +859,14 @@ public:
             case ProcessVariable::Type::integer:
                 // printf ("int\n");
                 {
-                    double *temp = int_to_double (dataPv->get_dimension (),
-                                                  dataPv->get_int_array ());
+                    double *temp;
+
+                    if (dataPv->get_specific_type().type == ProcessVariable::specificType::shrt)
+                        temp = to_double<short>(dataPv->get_dimension (),
+                                                dataPv->get_short_array ());
+                    else
+                        temp = to_double<int>(dataPv->get_dimension (),
+                                              dataPv->get_int_array ());
 #ifdef DEBUG
                     printf ("TwoDProfMon::execDef case int - calling widgetNewDispData\n");
 #endif
